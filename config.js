6cb4c673040c5dc37caca3a6569a23645ee13482
BeiboxxYT <103906106+BeiboxxYT@users.noreply.github.com>
2022-05-29 18:20:35 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
+  ['5219992095479', '👑 Beibox - Creador 👑', true],
+  ['50575008120', '🤖 Beibox - Bot 🤖', true],
   ['17722386341']
 ] // Cambia los numero que quieras
 global.mods = [] 
